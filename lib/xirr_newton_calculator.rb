522e0ed43ed8455ed5aa460cfa1bb6c29c377ef9
jpcolomer <jpcolomer@gmail.com>
2013-10-21 11:06:22 -0300

    Change Tabs to Spaces

--- lib/xirr_newton_calculator.rb
+++ lib/xirr_newton_calculator.rb
@@ -1,29 +1,29 @@
 class XirrNewtonCalculator
-	# Calculate Xirr using Newton Raphson
+  # Calculate Xirr using Newton Raphson
 
-	# Arguments
-	# 	flows: (Array)
-	# 	init_rate: (Fixnum, Bignum)
-	# 	max_iteration: (Fixnum)
+  # Arguments
+  #   flows: (Array)
+  #   init_rate: (Fixnum, Bignum)
+  #   max_iteration: (Fixnum)
 
 
-	EPS = 10 ** -7
+  EPS = 10 ** -8
 
-	attr_writer :f_xn
+  attr_writer :f_xn
 
-  def initialize(flows, init_rate, max_iteration=1000)
+  def initialize(flows, init_rate, max_iteration=10000)
    @flows = flows
    @x_n = init_rate
    @max_iteration = max_iteration
   end
 
   def calculate
-  	@f_xn = f(@x_n)
-  	iteration = 0
-  	while @f_xn.abs >= EPS && iteration <= @max_iteration
-  		@f_xn = f(@x_n)
-    	@x_n = next_value(@x_n)
-    	iteration += 1
+    @f_xn = f(@x_n)
+    iteration = 0
+    while @f_xn.abs >= EPS && iteration <= @max_iteration
+      @f_xn = f(@x_n)
+      @x_n = next_value(@x_n)
+      iteration += 1
     end
     @x_n
   end
@@ -33,24 +33,24 @@ class XirrNewtonCalculator
   # Argument X_n
   # Returns X_n+1
   def next_value(x)
-  	x - @f_xn.to_f/dfdx(x)
+    x - @f_xn.to_f/dfdx(x)
   end
 
   def dfdx(x)
-  	@flows[1..-1].inject(0) do |result, flow|
-  		diff_date = (Date.parse(flow.date.to_s) - Date.parse(@flows[0].date.to_s))/365
-  		result += flow.amount * (-diff_date) / ((1.0 + x) ** (diff_date + 1))
-  	end
+    @flows[1..-1].inject(0) do |result, flow|
+      diff_date = (Date.parse(flow.date.to_s) - Date.parse(@flows[0].date.to_s))/365
+      result += flow.amount * (-diff_date) / ((1.0 + x) ** (diff_date + 1))
+    end
   end
 
   def f(x)
-  	@f_xn = @flows.inject(0) do |result, flow|
+    @f_xn = @flows.inject(0) do |result, flow|
       diff_date = (Date.parse(flow.date.to_s) - Date.parse(@flows[0].date.to_s))/365
-  		result += flow.amount / ((1 + x) ** diff_date)
-  	end
+      result += flow.amount / ((1 + x) ** diff_date)
+    end
   end
 
   def discount_factor_to_irr (disc_fac)
-  	1.0/disc_fac - 1
+    1.0/disc_fac - 1
   end
 end
