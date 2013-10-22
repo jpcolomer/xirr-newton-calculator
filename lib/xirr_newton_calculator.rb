312723ec0cd78b5da0229ddda8e6c7e205bd8bbd
Klaus Hott <khott@cumplo.cl>
2013-10-22 11:32:32 -0300

    Performance boost

--- lib/xirr_newton_calculator.rb
+++ lib/xirr_newton_calculator.rb
@@ -6,24 +6,25 @@ class XirrNewtonCalculator
   #   init_rate: (Fixnum, Bignum)
   #   max_iteration: (Fixnum)
 
+  EPS = 10 ** -10
 
-  EPS = 10 ** -8
-
-  attr_writer :f_xn
-
-  def initialize(flows, init_rate, max_iteration=10000)
-   @flows = flows
-   @x_n = init_rate
-   @max_iteration = max_iteration
+  def initialize(flows, init_rate, max_iteration=10_000)
+    initial_date = Date.parse flows[0].date.to_s
+    @flows = flows.collect do |flow|
+      {
+        amount: flow.amount,
+        diff_date: (Date.parse(flow.date.to_s) - initial_date) / 365.0
+      }
+    end
+    @x_n = init_rate
+    @max_iteration = max_iteration
   end
 
   def calculate
-    @f_xn = f(@x_n)
-    iteration = 0
-    while @f_xn.abs >= EPS && iteration <= @max_iteration
-      @f_xn = f(@x_n)
+    f(@x_n)
+    @max_iteration.times do
+      break if @f_xn.abs < EPS
       @x_n = next_value(@x_n)
-      iteration += 1
     end
     @x_n
   end
@@ -33,24 +34,18 @@ class XirrNewtonCalculator
   # Argument X_n
   # Returns X_n+1
   def next_value(x)
-    x - @f_xn.to_f/dfdx(x)
+    x - f(x) / dfdx(x)
   end
 
   def dfdx(x)
     @flows[1..-1].inject(0) do |result, flow|
-      diff_date = (Date.parse(flow.date.to_s) - Date.parse(@flows[0].date.to_s))/365
-      result += flow.amount * (-diff_date) / ((1.0 + x) ** (diff_date + 1))
+      result += flow[:amount] * (-flow[:diff_date]) / ((1.0 + x) ** (flow[:diff_date] + 1.0))
     end
   end
 
   def f(x)
     @f_xn = @flows.inject(0) do |result, flow|
-      diff_date = (Date.parse(flow.date.to_s) - Date.parse(@flows[0].date.to_s))/365
-      result += flow.amount / ((1 + x) ** diff_date)
+      result += flow[:amount] / ((1.0 + x) ** flow[:diff_date])
     end
-  end
-
-  def discount_factor_to_irr (disc_fac)
-    1.0/disc_fac - 1
   end
 end
